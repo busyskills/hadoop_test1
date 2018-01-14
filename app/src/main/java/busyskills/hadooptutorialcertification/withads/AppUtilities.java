cbeb3e37241f553c8159f955997c5424820f66ff
paras301 <email2paras@gmail.com>
2018-01-13 20:53:15 -0500

    Added Ads function

--- app/src/main/java/busyskills/hadooptutorialcertification/withads/AppUtilities.java
+++ app/src/main/java/busyskills/hadooptutorialcertification/withads/AppUtilities.java
@@ -1,15 +1,20 @@
 package busyskills.hadooptutorialcertification.withads;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 /**
  * Created by email on 1/13/2018.
  */
 
-public class AppUtilities extends AppCompatActivity {
+public class AppUtilities {
+
     public static void showFragment(Fragment fragment, FragmentManager fragmentManager) {
         if(fragment != null && fragmentManager != null) {
             //FragmentManager fragmentManager = drawerActivity.getSupportFragmentManager();
@@ -17,6 +22,16 @@ public class AppUtilities extends AppCompatActivity {
             fragmentTransaction.replace(R.id.screen_area, fragment);
             fragmentTransaction.commit();
         }
+    }
+
+    public static void showBannerAd(FragmentActivity fragmentActivity) {
+        AdView mAdView;
+
+        MobileAds.initialize(fragmentActivity, fragmentActivity.getString(R.string.app_id));
+
+        mAdView = (AdView) fragmentActivity.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
 }
